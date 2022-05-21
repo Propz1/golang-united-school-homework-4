caa8773c75f8f09586994ae121be7759de32c8b3
Pavel Zhuravlev <propz@mail.ru>
2022-05-21 05:40:19 -0500

    update the string_sum.go file

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,17 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	plus    = "+"
+	minus   = "-"
+	blank   = " "
+	numbers = "0123456789"
+	n       = 2 //Number of numbers
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -10,6 +21,14 @@ var (
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+	sign                bool
+	value               bool
+	contains            bool
+	sum                 int
+	gap                 bool
+
+	str     = make([]string, 0, 5)
+	builder strings.Builder
 )
 
 // Implement a function that computes the sum of two int numbers written as a string
@@ -23,5 +42,116 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+
+	intput1 := strings.TrimSpace(input)
+
+	//is blank
+	if intput1 == "" {
+		err := fmt.Errorf("the string is blank: \"%w\"", errorEmptyInput)
+		return "", err
+	}
+
+	for _, r := range intput1 {
+
+		contains = strings.ContainsRune(numbers, r)
+
+		symbol := string(r)
+
+		if symbol == plus || symbol == minus {
+
+			if sign {
+				prev_sign := str[len(str)-1]
+				err := fmt.Errorf("two signs %s successively", "\""+prev_sign+symbol+"\"")
+				return "", err
+			}
+
+			if value {
+				str = append(str, ";", symbol)
+			} else {
+				str = append(str, symbol)
+			}
+
+			sign = true
+			value = false
+			gap = false
+			continue
+		}
+
+		if contains {
+
+			if len(str) > 0 {
+
+				pre_simbol := str[len(str)-1]
+
+				if pre_simbol != plus && pre_simbol != minus && gap {
+					if n == 2 {
+						err := fmt.Errorf("two number successively without sign: \"%w\"", errorNotTwoOperands)
+						return "", err
+					} else {
+						err := fmt.Errorf("two number successively without sign")
+						return "", err
+					}
+				}
+			}
+
+			str = append(str, string(r))
+
+			value = true
+			sign = false
+			gap = false
+
+			continue
+		}
+
+		if symbol == blank {
+			gap = true
+		}
+
+		if symbol != plus && symbol != minus && symbol != blank && !contains {
+			err := fmt.Errorf("\"%s\" is invalid character", string(r))
+			return "", err
+		}
+
+	}
+
+	for _, w := range str {
+		builder.WriteString(w)
+	}
+
+	st := builder.String()
+
+	stsplit := strings.Split(st, ";")
+
+	с := 0
+
+	for _, p := range stsplit {
+		fmt.Println(p)
+		i, err := strconv.Atoi(p)
+		if err != nil {
+			if n == 2 {
+				err := fmt.Errorf("\"%s\" symbol can't be converted to Int: \"%w\"", p, errorNotTwoOperands)
+				return "", err
+			} else {
+				panic(err)
+			}
+
+		}
+
+		с++
+		sum += i
+	}
+
+	if с == 1 {
+		err := fmt.Errorf("the string contains only one numbers \"%s\"", strconv.Itoa(sum))
+		return "", err
+	}
+
+	if с > n {
+		err := fmt.Errorf("the string contains more than %s numbers", strconv.Itoa(n))
+		return "", err
+	}
+
+	output = strconv.Itoa(sum)
+
+	return output, nil
 }
